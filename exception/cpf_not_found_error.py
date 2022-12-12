75bb25bea38a1efd9459dc7f8e966e4d069ae27f
marcosrff15@gmail.com <marcosrff15@gmail.com>
2022-12-12 17:48:50 -0300

    Atualizando diagrama xml
    - Corrigindo classes de exceçoes

--- exception/cpf_not_found_error.py
+++ exception/cpf_not_found_error.py
@@ -2,4 +2,4 @@
 class CpfNotFoundError(Exception):
     def __init__(self, mensagem):
         self.mensagem = mensagem
-        super().__init__(mensagem + "\n>>>A opção de menu digitada não existe!\n")
+        super().__init__(mensagem + "\n>>>Não há registro com este cpf\n")
